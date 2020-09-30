cd26016ddce7f086900a2b6307d9fe1f06cb5904
Reuben Ninan <intelphysic@gmail.com>
2020-09-30 06:48:12 -0500

    argparse module

--- src/app.py
+++ src/app.py
@@ -1,4 +1,13 @@
 import sys
+import argparse
 
-print("Main app")
-print(sys.argv)
+args = argparse.ArgumentParser()
+
+args.add_argument("-f","--folder", required=True, help="Folder that will be encrypted/decrypted")
+args.add_argument("-t","--type", required=True, help="Algorithm type selection")
+args.add_argument("-s","--secrets", required=False, help="n-tuple input used for key secrets and other parameters")
+args.add_argument("-a","--action", required=True, help="encrypt or decrypt")
+
+args_dict = vars(args.parse_args())
+
+print('COMMAND LINE ARGUMENTS: ' , args_dict)
