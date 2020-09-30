17ec8d42a719102f9e5395d9580b575d2b7b6079
Reuben Ninan <intelphysic@gmail.com>
2020-09-30 07:05:51 -0500

    algorithm structure init

---
+++ src/algos/algo1.py
@@ -0,0 +1,9 @@
+
+class Algo1:
+    def __init__(self, test_str):
+        self.test_str = test_str
+
+    def print(self):
+        print(self.test_str)
+        print("ALGO1")
+
--- src/app.py
+++ src/app.py
@@ -1,13 +1,24 @@
 import sys
 import argparse
 
+from algos import algo1
+from algos import algo2
+
 args = argparse.ArgumentParser()
 
-args.add_argument("-f","--folder", required=True, help="Folder that will be encrypted/decrypted")
-args.add_argument("-t","--type", required=True, help="Algorithm type selection")
+a1 = algo1.Algo1('Init String 1')
+a2 = algo2.Algo2('Init String 2')
+
+a1.print()
+a2.print()
+
+# Make all of these required when modules are fully implemented
+args.add_argument("-f","--folder", required=False, help="Folder that will be encrypted/decrypted")
+args.add_argument("-t","--type", required=False, help="Algorithm type selection")
 args.add_argument("-s","--secrets", required=False, help="n-tuple input used for key secrets and other parameters")
-args.add_argument("-a","--action", required=True, help="encrypt or decrypt")
+args.add_argument("-a","--action", required=False, help="encrypt or decrypt")
 
 args_dict = vars(args.parse_args())
 
-print('COMMAND LINE ARGUMENTS: ' , args_dict)
+print('COMMAND LINE ARGUMENTS: ', args_dict)
+
