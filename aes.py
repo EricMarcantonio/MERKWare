bbea541de9f685718df519b8a2d320b88869271e
emlabs <emlabs@localhost.localdomain>
2020-10-02 14:42:49 -0400

    added fix for smaller then 16 bytes

--- aes.py
+++ aes.py
@@ -4,7 +4,7 @@ block = [[], [], [], []]
 Using a with statement autocloses the file after opening
 the "rb" means to open the file for reading only, and open it in binary mode
 '''
-with open("./test_files/GovSecrets.docx", "rb") as f:
+with open("./test_files/pic.txt", "rb") as f:
     #  f.read(16) takes the first 16 bytes
     byte = f.read(16)
 
@@ -20,8 +20,16 @@ with open("./test_files/GovSecrets.docx", "rb") as f:
         block[1] = [hex_byte[8:10], hex_byte[10:12], hex_byte[12:14], hex_byte[14:16]]
         block[2] = [hex_byte[16:18], hex_byte[18:20], hex_byte[20:22], hex_byte[22:24]]
         block[3] = [hex_byte[24:26], hex_byte[26:28], hex_byte[28:30], hex_byte[30:32]]
-        # print(hex_byte)
-        # print(block)
+
+        #in case the file is less then 16 bytes (i.e a text file that has like 1 word)
+        for i in range(0,4):
+            for j in range(0,4):
+                if(block[i][j] == ""):
+                    block[i][j] = "00"
+
+        print(hex_byte)
+        print(block)
 
         #reset byte to be the NEXT 16 bytes (something not explicit)
         byte = f.read(16)
+
