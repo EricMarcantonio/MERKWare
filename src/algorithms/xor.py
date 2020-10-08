627110a424c4ee0bfaebb30c2bd4f1ed7d43d656
Eric Marcantonio <eric11marcantonio@gmail.com>
2020-10-07 22:53:07 -0400

    Ended up having to do XOR before I was able to do AES (duh) Its pretty much done, just need some clean up. This should be double checked later

--- src/algorithms/xor.py
+++ src/algorithms/xor.py
@@ -1,4 +1,43 @@
+from src.utils.data_management import File, Folder
+import os
+
+
+def xor_bytes(w, x):
+    return bytes(i ^ j for i, j in zip(w, x))
+
+
 class XOR:
-    def __init__(self):
-        pass
-        # init
+    file: File or Folder = None
+    key: str = None
+    new_file: File or Folder = None
+
+    def __init__(self, filename: str, key: str):
+        self.key = key
+        if os.path.exists(filename):
+            if os.path.isdir(filename):
+                self.file = Folder(filename)
+                self.file.zip_folder()
+                self.file = File(self.file.folder_zipped_name)
+
+            if os.path.isfile(filename):
+                self.file = File(filename)
+                if not self.file.file_path_raw.endswith(".merk"):
+                    self.new_file = self.file.encrypted_file_stream()
+                else:
+                    self.new_file = self.file.decrypted_file_stream()
+            else:
+                raise FileNotFoundError
+        else:
+            raise FileExistsError
+
+    def crypt(self):
+        total_bytes = self.file.read_file().read()
+        while len(bytes(self.key, encoding='ascii')) < len(total_bytes):
+            self.key *= 2
+        key_bytes = bytes(self.key, encoding='ascii')
+        self.new_file.write(xor_bytes(total_bytes, key_bytes))
+        self.file.delete()
+
+
+lol = XOR("../../spec/test_files/secrets.txt.merk", "eric")
+lol.crypt()
