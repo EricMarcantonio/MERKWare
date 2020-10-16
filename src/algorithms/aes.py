bb4c8fbf2f3ee541971da2cb96aaf0a031ac1658
Eric Marcantonio <eric11marcantonio@gmail.com>
2020-10-16 18:15:40 -0400

    works until round key 6, and I have no idea y

--- src/algorithms/aes.py
+++ src/algorithms/aes.py
@@ -39,12 +39,6 @@ inverted_s_box = (
 )
 
 
-def shift_rows(sub):
-    sub[0][1], sub[1][1], sub[2][1], sub[3][1] = sub[1][1], sub[2][1], sub[3][1], sub[0][1]
-    sub[0][2], sub[1][2], sub[2][2], sub[3][2] = sub[2][2], sub[3][2], sub[0][2], sub[1][2]
-    sub[0][3], sub[1][3], sub[2][3], sub[3][3] = sub[3][3], sub[0][3], sub[1][3], sub[2][3]
-
-
 def inverted_shift_rows(sub):
     sub[0][1], sub[1][1], sub[2][1], sub[3][1] = sub[3][1], sub[0][1], sub[1][1], sub[2][1]
     sub[0][2], sub[1][2], sub[2][2], sub[3][2] = sub[2][2], sub[3][2], sub[0][2], sub[1][2]
@@ -90,7 +84,6 @@ def mix_columns(state: list):
 
 
 def invert_mix_columns(state: list):
-    # see Sec 4.1.3 in The Design of Rijndael
     for i in range(4):
         a = fix_byte_80(fix_byte_80(state[i][0] ^ state[i][2]))
         b = fix_byte_80(fix_byte_80(state[i][1] ^ state[i][3]))
@@ -162,18 +155,6 @@ def new_sub(byte):
     return int(hex(s_box[hex_conversion[b0] * 16 + hex_conversion[b1]]), base=16)
 
 
-def g(matrix_4: bytearray):
-    # Swap
-    matrix_4.append(matrix_4.pop(0))
-    # SubBytes
-    for x in range(0, 4):
-        matrix_4[x] = new_sub(matrix_4[x])
-    # RCON
-    matrix_4[0] ^= round_constant[0]
-
-    return matrix_4
-
-
 def gmul(a: int, b: int):
     p = 0
     while a and b:
@@ -184,23 +165,8 @@ def gmul(a: int, b: int):
         else:
             a <<= 1
         b >>= 1
-    print(bin(p))
+
     return p
-
-
-def expand_128_key(key: bytearray):
-    # print(key.hex().upper())
-    k = [bytearray() for _ in range(0, 8)]
-    k[0] = key[0:4]
-    k[1] = key[4:8]
-    k[2] = key[8:12]
-    k[3] = key[12:16]
-    k[4] = xor_bytes(k[0], g(key[12:16]))
-    k[5] = xor_bytes(k[4], k[1])
-    k[6] = xor_bytes(k[5], k[2])
-    k[7] = xor_bytes(k[6], k[3])
-
-    return k[4::]
 
 
 def print_matrix(matrix: list):
@@ -238,26 +204,26 @@ def from_matrix_to_bytearray(matrix: list) -> bytearray:
 
 def printf(text: str):
     for x in range(0, len(text), 2):
-        print(text[x: x + 1], end=" ")
+        print(text[x: x + 2].upper(), end=" ")
     print()
 
 
-def inc_bytes(a):
-    """ Returns a new byte array with the value increment by 1 """
-    out = a
+def sub_bytes(state):
+    for x in range(0, 4):
+        state[x] = bytearray(new_sub(w) for w in state[x])
 
-    if out == 0xFF:
-        out = 0
-    else:
-        out += 1
 
-    return out
+def shift_rows(state):
+    state[1] = bytearray([state[1][1], state[1][2], state[1][3], state[1][0]])
+    state[2] = bytearray([state[2][2], state[2][3], state[2][0], state[2][1]])
+    state[3] = bytearray([state[3][3], state[3][0], state[3][1], state[3][2]])
 
 
 class AES:
     byte_array: list = None
     key_byte_array: bytearray = None
     key_matrix: list = None
+    rcon_step = 0
 
     def __init__(self, byte_array: bytearray, secret_key: str):
         super().__init__()
@@ -265,49 +231,81 @@ class AES:
         self.key_byte_array = bytearray(secret_key, encoding=encoding)
         self.key_matrix = create_matrix(self.key_byte_array)
 
-    def encrypt(self):
-        state = self.key_matrix
-        # 0
+    def g(self, matrix_4: bytearray):
+        # Swap
+        # printf(matrix_4.hex().upper())
+        matrix_4.append(matrix_4.pop(0))
+        # printf(matrix_4.hex().upper())
 
-        state = [xor_bytes(state[x], self.byte_array[x]) for x in range(0, 4)]
-
-
-
-        # 1
         # SubBytes
         for x in range(0, 4):
-            state[x] = bytearray(new_sub(w) for w in state[x])
+            matrix_4[x] = new_sub(matrix_4[x])
+        # RCON
+        # printf(matrix_4.hex().upper())
 
-        # ShiftRows
-        state[1] = bytearray([state[1][1], state[1][2], state[1][3], state[1][0]])
-        state[2] = bytearray([state[2][2], state[2][3], state[2][0], state[2][1]])
-        state[3] = bytearray([state[3][3], state[3][0], state[3][1], state[3][2]])
+        matrix_4[0] ^= round_constant[self.rcon_step]
+        # printf(matrix_4.hex().upper())
+        self.rcon_step += 1
+        return matrix_4
 
-        # Mix Column -------------
-        state = flippy(state)
-        mix_columns(state)
-        state = flippy(state)
+    def expand_128_key(self, key: bytearray):
+        # print(key.hex().upper())
+        k = [bytearray() for _ in range(0, 8)]
+        k[0] = key[0:4]
+        k[1] = key[4:8]
+        k[2] = key[8:12]
+        k[3] = key[12:16]
+        k[4] = xor_bytes(k[0], self.g(key[12:16]))
+        k[5] = xor_bytes(k[4], k[1])
+        k[6] = xor_bytes(k[5], k[2])
+        k[7] = xor_bytes(k[6], k[3])
 
-        #  ----------
-        test = expand_128_key(self.key_byte_array)
-        test = flippy(test)
+        return k[4::]
 
-        state = [xor_bytes(state[x], test[x]) for x in range(0, 4)]
+    def encrypt(self):
+        state = self.key_matrix
+        # 0 XOR THE BYTES
+        keys_ready_to_be_xored = [self.key_byte_array]
 
-        # Sub bytes
-        for x in range(0, 4):
-            state[x] = bytearray(new_sub(w) for w in state[x])
+        for x in range(0, 9):
+            keys_ready_to_be_xored.append(from_matrix_to_bytearray(self.expand_128_key(keys_ready_to_be_xored[x])))
 
-        state[1] = bytearray([state[1][1], state[1][2], state[1][3], state[1][0]])
-        state[2] = bytearray([state[2][2], state[2][3], state[2][0], state[2][1]])
-        state[3] = bytearray([state[3][3], state[3][0], state[3][1], state[3][2]])
 
-        print_matrix(state)
-        state = flippy(state)
-        mix_columns(state)
-        state = flippy(state)
-        print_matrix(state)
-        #up to slide 9 without the round key.
+
+
+        '''
+        • Round 0: 54 68 61 74 73 20 6D 79 20 4B 75 6E 67 20 46 75
+        • Round 1: E2 32 FC F1 91 12 91 88 B1 59 E4 E6 D6 79 A2 93
+        • Round 2: 56 08 20 07 C7 1A B1 8F 76 43 55 69 A0 3A F7 FA
+        • Round 3: D2 60 0D E7 15 7A BC 68 63 39 E9 01 C3 03 1E FB
+        • Round 4: A1 12 02 C9 B4 68 BE A1 D7 51 57 A0 14 52 49 5B
+        • Round 5: B1 29 3B 33 05 41 85 92 D2 10 D2 32 C6 42 9B 69
+        
+        
+        • Round 6: BD 3D C2 B7 B8 7C 47 15 6A 6C 95 27 AC 2E 0E 4E
+        • Round 7: CC 96 ED 16 74 EA AA 03 1E 86 3F 24 B2 A8 31 6A
+        • Round 8: 8E 51 EF 21 FA BB 45 22 E4 3D 7A 06 56 95 4B 6C
+        • Round 9: BF E2 BF 90 45 59 FA B2 A1 64 80 B4 F7 F1 CB D8
+        • Round 10: 28 FD DE F8 6D A4 24 4A CC C0 A4 FE 3B 31 6F 26
+
+        '''
+
+
+        # # 1
+        state = [xor_bytes(state[x], keys_ready_to_be_xored[0]) for x in range(0, 4)]
+
+        for x in range(1, 9):
+            sub_bytes(state)
+            shift_rows(state)
+            state = flippy(state)
+            mix_columns(state)
+            state = flippy(state)
+            state = [xor_bytes(state[x], keys_ready_to_be_xored[x]) for x in range(0, 4)]
+
+        sub_bytes(state)
+        shift_rows(state)
+        state = [xor_bytes(state[x], keys_ready_to_be_xored[9]) for x in range(0, 4)]
+        printf(from_matrix_to_bytearray(state).hex())
 
         #           TESTING
 
