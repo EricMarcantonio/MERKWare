6c73e2a52d5141d55b2e370f433dfe44f74e7b5a
Reuben Ninan <intelphysic@gmail.com>
2020-10-19 15:49:35 -0400

    modified installation process

--- install.py
+++ install.py
@@ -1,15 +1,37 @@
 import os
+import sys
+import shutil
 import PyInstaller.__main__
 
 package_name = 'MERK'
 
-PyInstaller.__main__.run([
-    '--log-level=DEBUG',
-    '--name=%s' % package_name,
-    '--onefile',
-    '--clean',
-    '--icon=%s' % os.path.join('resources', 'icon.ico'),
-    os.path.join('src', 'app.py'),
-])
+def install():
+    PyInstaller.__main__.run([
+        '--log-level=WARN',
+        '--name=%s' % package_name,
+        '--onefile',
+        '--clean',
+        '--icon=%s' % os.path.join('resources', 'icon.ico'),
+        os.path.join('src', 'app.py'),
+    ])
+
+def del_folder(folder_name: str):
+    dir_path = (os.path.join(os.path.abspath(os.getcwd()),folder_name))
+    try:
+        shutil.rmtree(dir_path)
+    except OSError as e:
+        print("Error: %s : %s" % (dir_path, e.strerror))
+        print(f"{folder_name} folder either does not exist, or cannot be modified")
+
+if 'clean' in sys.argv:
+    print('Cleaning existing installation\n')
+    del_folder('build')
+    del_folder('dist')
+    print('/build and /dist directories removed')
+else:
+    print('INSTALLING...\n')
+    install()
+    print('Installation complete')
+
 
 # os.system("pyinstaller -n MERK --onefile src/app.py")
