02209b0b1608bd373b24908615a3ba7ce6f8ef30
Reuben Ninan <intelphysic@gmail.com>
2020-11-24 18:42:18 -0500

    finished ecc

--- src/algorithms/ecc.py
+++ src/algorithms/ecc.py
@@ -1,11 +1,15 @@
-
 from tinyec import registry
+import tinyec.ec as ec
 from Crypto.Cipher import AES
 import hashlib, secrets, binascii
+import json
+import base64
 
-
-class ECC:
+class Cipher:
     def __init__(self, curve="brainpoolP256r1"):
+        if curve is "" or curve is None:
+            self.curve = "brainpoolP256r1"
+
         curve_options = ["brainpoolP160r1",
                          "brainpoolP192r1",
                          "brainpoolP224r1",
@@ -22,13 +26,13 @@ class ECC:
         if curve in curve_options:
             self.curve = registry.get_curve(curve)
         else:
-            raise Exception("the specified curve is unknown.")
+            raise Exception("the specified curve is unknown",curve)
 
     def get_public_key(self, privateKey):
         publicKey = privateKey * self.curve.g
         return publicKey
 
-    def encrypt(self, msg, publicKey):
+    def encrypt_helper(self, msg, publicKey):
         ciphertextPrivateKey = secrets.randbelow(self.curve.field.n)
         sharedECCKey = ciphertextPrivateKey * publicKey
         sha = hashlib.sha256(int.to_bytes(sharedECCKey.x, 32, 'big'))
@@ -40,7 +44,7 @@ class ECC:
         ciphertextPublicKey = ciphertextPrivateKey * self.curve.g
         return (ciphertext, nonce, authTag, ciphertextPublicKey)
 
-    def decrypt(self, ciphertext, nonce, authTag, ciphertextPublicKey, privateKey):
+    def decrypt_helper(self, ciphertext, nonce, authTag, ciphertextPublicKey, privateKey):
         sharedECCKey = privateKey * ciphertextPublicKey
         sha = hashlib.sha256(int.to_bytes(sharedECCKey.x, 32, 'big'))
         sha.update(int.to_bytes(sharedECCKey.y, 32, 'big'))
@@ -49,18 +53,52 @@ class ECC:
         plaintext = aesCipher.decrypt_and_verify(ciphertext, authTag)
         return plaintext
 
+    def encrypt(self,cipher_bytes: bytes) -> bytes:
+        privateKey = secrets.randbelow(0xA9FB57DBA1EEA9BC3E660A909D838D718C397AA3B561A6F7901E0E82974856A7)
+        publicKey = self.get_public_key(privateKey)
+
+        ciphertext, nonce, authTag, ciphertextPubKey = self.encrypt_helper(cipher_bytes, publicKey)
+        keyDump = {
+            'nonce': (nonce).hex(),
+            'authTag': (authTag).hex(),
+            'x': int(ciphertextPubKey.x),
+            'y': int(ciphertextPubKey.y),
+            'privateKey': int(privateKey)
+        }
+        
+        key_file = open("ecc_key.json",'w')
+        json.dump(keyDump,key_file, indent=6)
+        key_file.close()
+        return ciphertext
+
+    def decrypt(self,cipher_bytes: bytes) -> bytes:
+        key_file = open("ecc_key.json",'r')
+        keyDetails = json.load(key_file)
+        key_file.close()
+        nonce = bytes.fromhex(keyDetails['nonce'])
+        authTag = bytes.fromhex(keyDetails['authTag'])
+
+        print(nonce)
+
+        ciphertextPubKey = ec.Point(self.curve, keyDetails['x'], keyDetails['y'])
+        privateKey = int(keyDetails['privateKey'])
+
+        decryptedMsg = self.decrypt_helper(cipher_bytes, nonce, authTag, ciphertextPubKey, privateKey)
+
+        return decryptedMsg 
+
 
 if __name__ == "__main__":
-    ecc = ECC()
+    ecc = Cipher()
 
     msg = b'This is a test'
     print("original msg:", msg)
-    privateKey = secrets.randbelow(0x321)
+    privateKey = secrets.randbelow(0xAAAAAAAAAAAAAAAAAAA)
     publicKey = ecc.get_public_key(privateKey)
 
-    ciphertext, nonce, authTag, ciphertextPubKey = ecc.encrypt(msg, publicKey)
+    ciphertext, nonce, authTag, ciphertextPubKey = ecc.encrypt_helper(msg, publicKey)
     print("encrypted msg:", ciphertext)
 
-    decryptedMsg = ecc.decrypt(ciphertext, nonce, authTag, ciphertextPubKey, privateKey)
+    decryptedMsg = ecc.decrypt_helper(ciphertext, nonce, authTag, ciphertextPubKey, privateKey)
     print("decrypted msg:", decryptedMsg)
 
